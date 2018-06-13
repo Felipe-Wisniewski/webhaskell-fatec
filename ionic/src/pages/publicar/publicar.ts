a805ead3ae3ea0d3c8ffaf7b15d4a6e3605f0bc3
felipecordeiro <felipecordeiro4@gmail.com>
2018-06-13 00:35:35 -0300

    publicar.ts this.artigosProvider.criaCurtida

--- ionic/src/pages/publicar/publicar.ts
+++ ionic/src/pages/publicar/publicar.ts
@@ -19,7 +19,13 @@ export class PublicarPage {
     this.navCtrl.push(FeedPage, { 'generos': this.categoriaid, 'titulo': this.titulo, 'texto': this.texto });
 
     this.artigosProvider.publicarArtigo(this.categoriaid, this.titulo, this.texto).then((result: any) => {
+      
+      this.artigosProvider.criaCurtida(result.resp).then((result: any) => { }).catch((error: any) => {
+        this.toast.create({ message: 'Erro ao criar curtida', position: 'botton', duration: 3000}).present();
+       });
+
       this.toast.create({ message: 'Artigo postado com sucesso!', position: 'botton', duration: 3000}).present();
+      this.navCtrl.push(PublicarPage);   
     })
     .catch((error: any) => {
       this.toast.create({ message: 'Erro ao publicar o artigo', position: 'botton', duration: 3000}).present();
@@ -28,4 +34,3 @@ export class PublicarPage {
   
 
 }
-
