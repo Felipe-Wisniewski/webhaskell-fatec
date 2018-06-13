a02349b28f34f6ae4771ce926b27707c8a0dbc16
Felipe Wisniewski <felipewisniewski@gmail.com>
2018-06-13 14:26:31 -0300

    login html

--- ionic/src/pages/user-login/user-login.ts
+++ ionic/src/pages/user-login/user-login.ts
@@ -1,7 +1,7 @@
 import { ArtigosProvider } from './../../providers/artigos/artigos';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
-import { ConfigurarPage } from '../configurar/configurar';
+import { TabsPage } from '../tabs/tabs';
 
 @IonicPage()
 @Component({
@@ -16,16 +16,13 @@ export class UserLoginPage {
   constructor(public navCtrl: NavController, public navParams: NavParams, private toast: ToastController, private artigosProvider: ArtigosProvider) { }
 
 pushParams() {
-  this.navCtrl.push(ConfigurarPage, { 'email': this.email, 'password': this.password });
-
-  this.artigosProvider.login(this.email, this.password).then((result: any) => {
-      this.toast.create({ message: 'Usuário~' + result.resp + 'logado com sucesso.', position: 'botton', duration: 3000 }).present();
-      //Salvar o token no Ionic Storage para usar em futuras requisições.
-      //Redirecionar o usuario para outra tela usando o navCtrl
-      //this.navCtrl.pop();
-      //this.navCtrl.setRoot()
-    })
-    .catch((error: any) => {
+  this.artigosProvider.getUsuarioEmail(this.email, this.password).then((result: any) => {
+    if(result.resp.password == this.password){
+      this.navCtrl.setRoot(TabsPage);
+    }else{
+      this.toast.create({ message: 'Senha inválida!', position: 'botton', duration: 3000 }).present();
+    }
+  }).catch((error: any) => {
       this.toast.create({ message: 'Erro ao efetuar login.', position: 'botton', duration: 3000 }).present();
     });
   }
